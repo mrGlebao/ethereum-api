f38ba7726e5a756224fcc4f09d86793339be5c63
Glebus <zaborovskij.gleb@gmail.com>
2019-06-14 23:19:46 +0300

    added some basic project files

---
+++ src/main/kotlin/com/wsd/glebus/ethereumapi/EthereumApiApplication.kt
@@ -0,0 +1,11 @@
+package com.wsd.glebus.ethereumapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EthereumApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<EthereumApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/wsd/glebus/ethereumapi/EthereumApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.wsd.glebus.ethereumapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class EthereumApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
