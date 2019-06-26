c72fa3172a012ca43efa9b8908bb3399c70b0b68
Gleb <zaborovskij.gleb@gmail.com>
2019-06-27 00:24:13 +0300

    wrapped embedded jar into docker container

--- src/main/kotlin/com/wsd/glebus/ethereumapi/EthereumApiApplication.kt
+++ src/main/kotlin/com/wsd/glebus/ethereumapi/EthereumApiApplication.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class EthereumApiApplication
-
-fun main(args: Array<String>) {
-	runApplication<EthereumApiApplication>(*args)
+class EthereumApiApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<EthereumApiApplication>(*args)
+        }
+    }
 }
